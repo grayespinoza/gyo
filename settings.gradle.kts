d72447d120a2397c9c344f728efa2dfccc9d47a1
Grey <grey@grayespinoza.dev>
2025-07-16 19:01:26 -0700

    refactor: restructure for multi-version support
    Closes #10, closes #11:

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+  repositories {
+    gradlePluginPortal()
+    mavenCentral()
+    maven("https://maven.fabricmc.net/")
+    maven("https://maven.kikugie.dev/snapshots") { name = "KikuGie Snapshots" }
+  }
+}
+
+plugins { id("dev.kikugie.stonecutter") version "0.7-beta.6" }
+
+stonecutter {
+  create(rootProject) {
+    versions("1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4", "1.21.5")
+    vcsVersion = "1.21.5"
+  }
+}
+
+rootProject.name = "gyo"
